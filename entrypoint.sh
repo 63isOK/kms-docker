d35861d7002583fdca874c69fa9a18bea20ec67f
63isOK <1876180681@qq.com>
2019-01-25 14:19:32 +0800

    更新turn配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
 set -e
 
+cd /home/yb/workspace/svn/kms-6.9.0/kms-omni-build/build-Release
+
 if [ -n "$KMS_TURN_URL" ]; then
-  echo "turnURL=$KMS_TURN_URL" > /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+  echo "turnURL=$KMS_TURN_URL" > ./config/kurento/WebRtcEndpoint.conf.ini
 fi
 
 # if [ -n "$KMS_STUN_IP" -a -n "$KMS_STUN_PORT" ]; then
@@ -16,7 +18,6 @@ cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
 
 # exec /usr/bin/kurento-media-server "$@"
 
-cd /home/yb/workspace/svn/kms-6.9.0/kms-omni-build/build-Release
 exec kurento-media-server/server/kurento-media-server \
     --modules-path=. \
     --modules-config-path=./config \
